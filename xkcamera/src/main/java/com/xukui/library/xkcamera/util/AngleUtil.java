2e9053847920f66ce319dc8fb88015c78ae1f7fc
xukui <152780382@qq.com>
2019-05-18 10:00:17 +0800

    init

--- xkcamera/src/main/java/com/xukui/library/xkcamera/util/AngleUtil.java
+++ xkcamera/src/main/java/com/xukui/library/xkcamera/util/AngleUtil.java
@@ -3,41 +3,25 @@ package com.xukui.library.xkcamera.util;
 public class AngleUtil {
 
     public static int getSensorAngle(float x, float y) {
-        if (Math.abs(x) > Math.abs(y)) {
-            /**
-             * 横屏倾斜角度比较大
-             */
-            if (x > 4) {
-                /**
-                 * 左边倾斜
-                 */
+        if (Math.abs(x) > Math.abs(y)) {//横屏倾斜角度比较大
+            if (x > 4) {//左边倾斜
                 return 270;
-            } else if (x < -4) {
-                /**
-                 * 右边倾斜
-                 */
+
+            } else if (x < -4) {//右边倾斜
                 return 90;
-            } else {
-                /**
-                 * 倾斜角度不够大
-                 */
+
+            } else {//倾斜角度不够大
                 return 0;
             }
+
         } else {
-            if (y > 7) {
-                /**
-                 * 左边倾斜
-                 */
+            if (y > 7) {//左边倾斜
                 return 0;
-            } else if (y < -7) {
-                /**
-                 * 右边倾斜
-                 */
+
+            } else if (y < -7) {//右边倾斜
                 return 180;
-            } else {
-                /**
-                 * 倾斜角度不够大
-                 */
+
+            } else {//倾斜角度不够大
                 return 0;
             }
         }
