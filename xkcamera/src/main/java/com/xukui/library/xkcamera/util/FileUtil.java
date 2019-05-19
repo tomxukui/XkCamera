e69e168919ea20389d663724b8862a347c0b3f79
xukui <152780382@qq.com>
2019-05-19 17:44:57 +0800

    init

--- xkcamera/src/main/java/com/xukui/library/xkcamera/util/FileUtil.java
+++ xkcamera/src/main/java/com/xukui/library/xkcamera/util/FileUtil.java
@@ -1,63 +1,22 @@
 package com.xukui.library.xkcamera.util;
 
-import android.graphics.Bitmap;
-import android.os.Environment;
+import android.text.TextUtils;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class FileUtil {
 
-    private static final File parentPath = Environment.getExternalStorageDirectory();
-    private static String storagePath = "";
-    private static String DST_FOLDER_NAME = "JCamera";
+    /**
+     * 删除文件
+     */
+    public static void deleteFile(String filePath) {
+        if (!TextUtils.isEmpty(filePath)) {
+            File file = new File(filePath);
 
-    private static String initPath() {
-        if (storagePath.equals("")) {
-            storagePath = parentPath.getAbsolutePath() + File.separator + DST_FOLDER_NAME;
-            File f = new File(storagePath);
-            if (!f.exists()) {
-                f.mkdir();
+            if (file != null && file.exists()) {
+                file.delete();
             }
         }
-        return storagePath;
-    }
-
-    public static String saveBitmap(String dir, Bitmap b) {
-        DST_FOLDER_NAME = dir;
-        String path = initPath();
-        long dataTake = System.currentTimeMillis();
-        String jpegName = path + File.separator + "picture_" + dataTake + ".jpg";
-        try {
-            FileOutputStream fout = new FileOutputStream(jpegName);
-            BufferedOutputStream bos = new BufferedOutputStream(fout);
-            b.compress(Bitmap.CompressFormat.JPEG, 100, bos);
-            bos.flush();
-            bos.close();
-            return jpegName;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "";
-        }
-    }
-
-    public static boolean deleteFile(String url) {
-        boolean result = false;
-        File file = new File(url);
-        if (file.exists()) {
-            result = file.delete();
-        }
-        return result;
-    }
-
-    public static boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
     }
 
 }
